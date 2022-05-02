7de419cfb0ddeeb45589e88473ee10741c82bee1
Maxim Voyevoda <maximvoyevoda@gmail.com>
2022-05-02 15:03:50 -0400

    under dev

--- max_final_project/main.cpp
+++ max_final_project/main.cpp
@@ -1,14 +1,18 @@
-//
-//  main.cpp
-//  max_final_project
-//
-//  Created by Maxim Voyevoda on 5/2/22.
-//
+#include<iostream>
+#include<iomanip>
+#include<string>
+using namespace std;
 
-#include <iostream>
+string calculateLetterGrade(double grade);
+double findMinQuiz(double q1, double q2, double q3, double q4);
+double findAverageQuizScore(double q1, double q2, double q3, double q4);
+double findAverageHomeworkScore(double h1, double h2, double h3, double h4, double h5, double h6, double h7);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main() {
+    
+    
+    
+    
     return 0;
 }
